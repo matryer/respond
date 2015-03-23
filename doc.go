22dbf12522c4849da325fece53dc03f4ff10d068
Mat Ryer <thatmatryer@gmail.com>
2015-03-23 14:53:22 +0000

    fixed subline

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
-// Package respond provides API responding capabilities.
+// Package respond provides idiomatic API responses for Go.
 //
 //     respond.With(http.StatusOK, data).To(w, r)
 package respond
